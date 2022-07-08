ffa4815856c4c2be54469ac21007dc3a7e645133
Tian Feng <tian.feng@saucelabs.com>
2022-07-08 10:27:09 -0700

    update allure-plugin

---
+++ examples/allure-plugin/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress')
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  env: {
+    issuePrefix: 'https://your.domain.atlassian.net/browse/',
+    tmsPrefix: 'https://some.testrail.instance/path/suite/caseID-',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      console.log("config: ", config)
+      return config;
+    },
+  },
+})
