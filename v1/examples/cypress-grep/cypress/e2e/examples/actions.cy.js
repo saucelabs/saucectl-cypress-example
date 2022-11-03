7a49c77ba4070d5c94d7412e249f00a277d948c9
Mike Han <mike.han@saucelabs.com>
2022-11-03 14:46:51 -0600

    fix tag

--- v1/examples/cypress-grep/cypress/e2e/examples/actions.cy.js
+++ v1/examples/cypress-grep/cypress/e2e/examples/actions.cy.js
@@ -2,7 +2,7 @@ context('Actions', () => {
   beforeEach(() => {
     cy.visit('https://example.cypress.io/commands/actions')
   })
-  it('.type() - type into a DOM element', { tags: ['smoke'] }, () => {
+  it('.type() - type into a DOM element', { tags: ['@smoke'] }, () => {
     cy.get('.action-email')
         .type('fake@email.com').should('have.value', 'fake@email.com')
   })
