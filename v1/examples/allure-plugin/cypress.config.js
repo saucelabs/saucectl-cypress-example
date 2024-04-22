b592b837f064d88440a41bf49f576059a53ce6cb
Mike Han <56001373+mhan83@users.noreply.github.com>
2024-04-22 14:14:42 -0600

    fix: Use the new allure reporter plugin (#108)
    * Use the new allure reporter plugin
    
    * Verify allure artifacts were created and downloaded

--- v1/examples/allure-plugin/cypress.config.js
+++ v1/examples/allure-plugin/cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('cypress')
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   env: {
@@ -8,7 +8,9 @@ module.exports = defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
+      allureCypress(on, {
+        resultsDir: './allure-results',
+      });
       return config;
     },
   },
