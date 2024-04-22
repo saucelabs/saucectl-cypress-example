642b1a27f62c4e8b07bf6186416189dbeeda0929
Mike Han <mike.han@saucelabs.com>
2024-04-22 11:47:29 -0600

    Use the new allure reporter plugin

--- v1/examples/allure-plugin/cypress/support/e2e.js
+++ v1/examples/allure-plugin/cypress/support/e2e.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import '@shelex/cypress-allure-plugin';
+import "allure-cypress/commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
