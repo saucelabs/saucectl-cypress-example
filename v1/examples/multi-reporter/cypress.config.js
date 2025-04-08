97c7a8cc60cc3daff6ff5eb777e61b7bc9d947da
Mike Han <mike.han@saucelabs.com>
2025-04-08 16:07:15 -0600

    cleanup

--- v1/examples/multi-reporter/cypress.config.js
+++ v1/examples/multi-reporter/cypress.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   reporter: "cypress-multi-reporters",
-
   reporterOptions: {
     configFile: "reporter-config.json",
   },
-
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
