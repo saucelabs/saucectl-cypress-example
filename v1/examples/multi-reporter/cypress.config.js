b9826fc1db3d04af927d5b0f479e1a793460949a
Mike Han <mike.han@saucelabs.com>
2025-04-08 15:01:08 -0600

    docs: Update multireporter example to use cypress config exclusively

--- v1/examples/multi-reporter/cypress.config.js
+++ v1/examples/multi-reporter/cypress.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    saveJson: false,
+    ignoreVideos: true, //  Must be ignored, the plugin will fail otherwise. Videos are enabled on Sauce by default.
+    reportDir: '__assets__/cypress/report',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
